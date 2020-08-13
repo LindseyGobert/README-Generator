9b2ccba67f4a6acad9a8efb6cd3fb3433900d9f1
Lindsey Gobert <lmgobert1@gmail.com>
2020-08-13 13:39:23 -0500

    made changes to fix the README

--- README.md
+++ README.md
@@ -1,38 +1,44 @@
  
-    # README generator
+# README generator
 
-    ## Description
-   
-    Application to generate README's faster
-    
-    ##Table of Contents
-    -[Installation](#Installation)
-    -[Usage](#usage)
-    -[Guideline](#guidelines)
-    -[Test](#test)
-    -[License](#license)
-    -[Questions](#email)
-    
-    ## Installation
-    
-    n/a
-    
-    ## Usage
-    
-    n/a
-    
-    ## License
-    
-    This project is licensed under MIT.![License](https://img.shields.io/npm/l/inquirer)
-    
-    ## Contributing
-    
-    n/a
-    
-    ## Test
-    
-    n/a
-    
-    ## Questions
-    For any questions, please email me at lmgobert1@gmail.com. 
-    You can also check out more of my work on my github page. User name is LindseyGobert
+## Description
+
+An application to help generate README's faster.
+
+##Table of Contents
+
+-[Installation](#Installation)
+
+-[Usage](#usage)
+
+-[Guideline](#guidelines)
+
+-[Test](#test)
+
+-[License](#license)
+
+-[Questions](#email)
+
+## Installation
+
+n/a
+
+## Usage
+
+n/a
+
+## License
+
+This project is licensed under MIT. ![License](https://img.shields.io/npm/l/inquirer)
+
+## Contributing
+
+n/a
+
+## Test
+
+n/a
+
+## Questions
+For any questions, please email me at lmgobert1@gmail.com. 
+You can also check out more of my work on my github page. User name is LindseyGobert
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,42 +1,49 @@
 function generateMarkdown(responses) {
     return ` 
-    # ${responses.title}
+# ${responses.title}
 
-    ## Description
-   
-    ${responses.description}
-    
-    ##Table of Contents
-    -[Installation](#Installation)
-    -[Usage](#usage)
-    -[Guideline](#guidelines)
-    -[Test](#test)
-    -[License](#license)
-    -[Questions](#email)
-    
-    ## Installation
-    
-    ${responses.installation}
-    
-    ## Usage
-    
-    ${responses.usage}
-    
-    ## License
-    
-    This project is licensed under ${responses.license}.![License](https://img.shields.io/npm/l/inquirer)
-    
-    ## Contributing
-    
-    ${responses.guidelines}
-    
-    ## Test
-    
-    ${responses.test}
-    
-    ## Questions
-    For any questions, please email me at ${responses.email}. 
-    You can also check out more of my work on my github page. User name is ${responses.github}`;
+## Description
+
+${responses.description}
+
+##Table of Contents
+
+-[Installation](#Installation)
+
+-[Usage](#usage)
+
+-[Guideline](#guidelines)
+
+-[Test](#test)
+
+-[License](#license)
+
+-[Questions](#email)
+
+## Installation
+
+${responses.installation}
+
+## Usage
+
+${responses.usage}
+
+## License
+
+This project is licensed under ${responses.license}. ![License](https://img.shields.io/npm/l/inquirer)
+
+## Contributing
+
+${responses.guidelines}
+
+## Test
+
+${responses.test}
+
+## Questions
+For any questions, please email me at ${responses.email}. 
+You can also check out more of my work on my github page. User name is ${responses.github}
+`;
   }
   
   module.exports = generateMarkdown;
